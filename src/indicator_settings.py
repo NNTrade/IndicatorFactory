d883325f879f6bf962599a7e4b3fa06887922596
insonusk <insonus.k@gmail.com>
2023-02-28 21:37:12 +0300

    add __str__, extract common functions

--- src/indicator_settings.py
+++ src/indicator_settings.py
@@ -1,58 +1,66 @@
 from __future__ import annotations
+from abc import ABC
 from typing import Dict
 
-class IndicatorSettings:
-  TYPE_DICT_FIELD = "type"
-  PARAMETERS_DICT_FIELD = "parameters"
 
-  def __init__(self, indicator_type: str, parameters: Dict[str, float]) -> None:
-    self._indicator_type = indicator_type
-    self._parameters = parameters.copy()
-    self.__hash = hash(self._indicator_type) * hash(frozenset(self.parameters.items()))
-    pass
+class __baseSettings(ABC):
+    def __init__(self, indicator_type: str, parameters: Dict[str, float]) -> None:
+        self._indicator_type = indicator_type
+        self._parameters = parameters.copy()
 
-  @property
-  def indicator_type(self) -> str:
-    return self._indicator_type
+    def to_dict(self) -> Dict[str, any]:
+        return {
+            IndicatorSettings.TYPE_DICT_FIELD: self._indicator_type,
+            IndicatorSettings.PARAMETERS_DICT_FIELD: self._parameters.copy()
+        }
 
-  @property
-  def parameters(self) -> Dict[str, float]:
-    return self._parameters.copy()
+    def __str__(self) -> str:
+        return str(self.to_dict())
 
-  def __hash__(self):
-    return self.__hash
+    def __eq__(self, other: __baseSettings):
+        if self.__class__ != other.__class__:
+            return False
+        return self._indicator_type == other._indicator_type and self._parameters == other._parameters
 
-  def __eq__(self, other: IndicatorSettings):
-    if self.__class__ != other.__class__:
-      return False
-    return self.indicator_type == other.indicator_type and self.parameters == other.parameters
+class IndicatorSettings(__baseSettings):
+    TYPE_DICT_FIELD = "type"
+    PARAMETERS_DICT_FIELD = "parameters"
 
-  @staticmethod
-  def from_dict(config: Dict[str, any]) -> IndicatorSettings:
-    return IndicatorSettings(config[IndicatorSettings.TYPE_DICT_FIELD], config[IndicatorSettings.PARAMETERS_DICT_FIELD])
-
-  def to_dict(self) -> Dict[str, any]:
-    return {
-        IndicatorSettings.TYPE_DICT_FIELD: self._indicator_type,
-        IndicatorSettings.PARAMETERS_DICT_FIELD: self.parameters
-    }
-
-  def copy(self)->IndicatorSettings:
-    return IndicatorSettings(self._indicator_type, self._parameters.copy())
-
-class IndicatorSettingsBuilder(IndicatorSettings):
-
-    @staticmethod
-    def from_dict(config: Dict[str, any]) -> IndicatorSettingsBuilder:
-       return IndicatorSettingsBuilder(config[IndicatorSettings.TYPE_DICT_FIELD], config[IndicatorSettings.PARAMETERS_DICT_FIELD])
-    
-    def __init__(self, indicator_type: str = None,
-                 args: Dict[str, float] = {}) -> None:
-        super().__init__(indicator_type, args.copy())
+    def __init__(self, indicator_type: str, parameters: Dict[str, float]) -> None:
+        super().__init__(indicator_type, parameters)
+        self.__hash = hash(self._indicator_type) * \
+            hash(frozenset(self.parameters.items()))
+        pass
 
     @property
     def indicator_type(self) -> str:
-        return super().indicator_type
+        return self._indicator_type
+
+    @property
+    def parameters(self) -> Dict[str, float]:
+        return self._parameters.copy()
+
+    def __hash__(self):
+        return self.__hash
+
+    @staticmethod
+    def from_dict(config: Dict[str, any]) -> IndicatorSettings:
+        return IndicatorSettings(config[IndicatorSettings.TYPE_DICT_FIELD], config[IndicatorSettings.PARAMETERS_DICT_FIELD])
+
+
+class IndicatorSettingsBuilder(__baseSettings):
+
+    @staticmethod
+    def from_dict(config: Dict[str, any]) -> IndicatorSettingsBuilder:
+        return IndicatorSettingsBuilder(config[IndicatorSettings.TYPE_DICT_FIELD], config[IndicatorSettings.PARAMETERS_DICT_FIELD])
+
+    def __init__(self, indicator_type: str = None,
+                 parameters: Dict[str, float] = {}) -> None:
+        super().__init__(indicator_type, parameters)
+
+    @property
+    def indicator_type(self) -> str:
+        return self._indicator_type
 
     @indicator_type.setter
     def indicator_type(self, new_value: str) -> float:
@@ -80,3 +88,6 @@ class IndicatorSettingsBuilder(IndicatorSettings):
 
     def build(self) -> IndicatorSettings:
         return IndicatorSettings(self.indicator_type, self.parameters)
+
+    def copy(self) -> IndicatorSettingsBuilder:
+        return IndicatorSettingsBuilder(self._indicator_type, self._parameters.copy())
